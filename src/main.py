eede318228017a4954aa68919bce8eab921435e2
Maaz Imam <maazimam03@gmail.com>
2023-08-19 21:33:10 +0500

    Attempted Interview

--- src/main.py
+++ src/main.py
@@ -1,22 +1,5 @@
 import constants
-
-from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-
-from langchain.document_loaders import PyMuPDFLoader
-from langchain.document_loaders import Docx2txtLoader
-from langchain.document_loaders import TextLoader
-
-import json
-import textwrap
-import os
-import random
-import string
+from libFile import *
 
 os.environ["OPENAI_API_KEY"] = constants.OPENAI_API_KEY
 embeddings = OpenAIEmbeddings()
@@ -135,5 +118,3 @@ def make_json_from_resume(pdf_url, print=True):
 if __name__ == "__main__":
     url = "AshadAbdullah_resume.docx"
     make_json_from_resume(url, print=False)
-
-
